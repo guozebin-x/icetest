ca9b5bcb5473786df674b458aac711b05d1e1a30
guozebin <guozebin@meicai.cn>
2019-12-02 16:35:50 +0800

    asds

--- src/pages/Charts/components/OverviewChartCard/ColumnChart.jsx
+++ src/pages/Charts/components/OverviewChartCard/ColumnChart.jsx
@@ -1,25 +1,25 @@
-import React from 'react';
-import { Chart, Geom, Tooltip } from 'bizcharts';
-import PropTypes from 'prop-types';
+import React from "react";
+import { Chart, Geom, Tooltip } from "bizcharts";
+import PropTypes from "prop-types";
 
 export default function ColumnChart(props) {
   const { color } = props;
   const data = [
-    { month: '1', visits: 38 },
-    { month: '2', visits: 52 },
-    { month: '3', visits: 61 },
-    { month: '4', visits: 50 },
-    { month: '5', visits: 65 },
-    { month: '6', visits: 60 },
-    { month: '7', visits: 60 },
-    { month: '8', visits: 58 },
-    { month: '9', visits: 48 },
-    { month: '10', visits: 50 },
-    { month: '11', visits: 40 },
-    { month: '12', visits: 40 },
+    { month: "1", visits: 381 },
+    { month: "2", visits: 52 },
+    { month: "3", visits: 61 },
+    { month: "4", visits: 50 },
+    { month: "5", visits: 65 },
+    { month: "6", visits: 60 },
+    { month: "7", visits: 60 },
+    { month: "8", visits: 58 },
+    { month: "9", visits: 48 },
+    { month: "10", visits: 50 },
+    { month: "11", visits: 40 },
+    { month: "12", visits: 40 }
   ];
   const cols = {
-    visits: { tickInterval: 20 },
+    visits: { tickInterval: 20 }
   };
   return (
     <Chart
@@ -30,20 +30,20 @@ export default function ColumnChart(props) {
       scale={cols}
     >
       <Geom
-        type={props.type || 'interval'}
+        type={props.type || "interval"}
         position="month*visits"
-        color={color || '#2077ff'}
+        color={color || "#2077ff"}
         shape="smooth"
       />
-      <Tooltip crosshairs={{ type: 'y' }} />
+      <Tooltip crosshairs={{ type: "y" }} />
     </Chart>
   );
 }
 
 ColumnChart.propTypes = {
-  type: PropTypes.string,
+  type: PropTypes.string
 };
 
 ColumnChart.defaultProps = {
-  type: 'interval',
+  type: "interval"
 };
